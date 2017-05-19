34147d717a821f5de3a14b5dd6d57a218b8b44eb
Jiiks <jiiks@windowslive.com>
2017-05-20 00:44:00 +0300

    Game class

---
+++ PoE-TradeUI/poe/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace PoE_TradeUI.poe {
+
+    public class Game {
+
+        private Process _poeProcess;
+        private IntPtr _poeHandle = IntPtr.Zero;
+        private Thread _thread;
+
+        public Game() {
+            _poeProcess = FindGame();
+            if(_poeProcess != null) _poeHandle = _poeProcess.MainWindowHandle;
+            _thread = new Thread(GameThread) {IsBackground = true};
+            _thread.Start();
+        }
+
+        private static Process FindGame() {
+            return (from process in Process.GetProcesses() let lower = process.ProcessName.ToLower() where lower.Contains("path") && lower.Contains("exile") select process).FirstOrDefault();
+        }
+
+        private void GameThread() {
+            while (true) {
+                if (_poeProcess == null) _poeProcess = FindGame();
+                if (_poeProcess == null) {
+                    Thread.Sleep(100);
+                    continue;
+                }
+                if (_poeHandle == IntPtr.Zero) _poeHandle = _poeProcess.MainWindowHandle;
+                if(_poeHandle == IntPtr.Zero) continue;
+
+                Native.Rect rect;
+                if(Native.GetWindowRect(_poeHandle, out rect)){
+                    //Emit event
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
