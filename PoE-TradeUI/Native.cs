4527dedbdbea64006b02e1719a962e42ee2b7394
Jiiks <jiiks@windowslive.com>
2017-05-20 00:35:24 +0300

    Init

---
+++ PoE-TradeUI/Native.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PoE_TradeUI {
+
+    public static class Native {
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+    }
+}
