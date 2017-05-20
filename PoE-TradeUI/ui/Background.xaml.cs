f68b39d7224d05dc2b18219bbb225d547ff386d0
Jiiks <jiiks@windowslive.com>
2017-05-21 02:55:35 +0300

    Temp banner

--- PoE-TradeUI/ui/Background.xaml.cs
+++ PoE-TradeUI/ui/Background.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -7,7 +8,8 @@ namespace PoE_TradeUI.ui {
 
     public partial class Background  {
 
-       private readonly Image _backgroundPattern = new Image("bg-pattern");
+        private readonly Image _backgroundPattern = new Image("bg-pattern");
+        private readonly Image _banner = new Image("banner-test");
 
         public Background() {
             InitializeComponent();
@@ -19,6 +21,14 @@ namespace PoE_TradeUI.ui {
             for (var y = 0; y < bgCount; y++) {
                 context.DrawImage(_backgroundPattern.BitmapImage, new Rect(0, y * _backgroundPattern.Height - y*1, ActualWidth, _backgroundPattern.Height));
             }
+
+            var bannerHeight = ActualHeight * .08;
+
+            context.DrawImage(_banner.BitmapImage, new Rect(0,0, ActualWidth, bannerHeight));
+            var tradeUiText = new FormattedText("TradeUI", CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                new Typeface("Arial"), 20, Brushes.White);
+            
+            context.DrawText(tradeUiText, new Point(ActualWidth / 2 - tradeUiText.Width / 2, bannerHeight / 2));
         }
     }
 }
