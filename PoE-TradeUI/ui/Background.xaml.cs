2e60381a0a31f0455cad5823d5c9060eed93f36b
Jiiks <jiiks@windowslive.com>
2017-05-21 03:07:00 +0300

    Draw border

--- PoE-TradeUI/ui/Background.xaml.cs
+++ PoE-TradeUI/ui/Background.xaml.cs
@@ -10,6 +10,7 @@ namespace PoE_TradeUI.ui {
 
         private readonly Image _backgroundPattern = new Image("bg-pattern");
         private readonly Image _banner = new Image("banner-test");
+        private readonly Image _borderLeft = new Image("border-left");
 
         public Background() {
             InitializeComponent();
@@ -19,16 +20,22 @@ namespace PoE_TradeUI.ui {
             var bgCount = ActualHeight / _backgroundPattern.Height;
 
             for (var y = 0; y < bgCount; y++) {
-                context.DrawImage(_backgroundPattern.BitmapImage, new Rect(0, y * _backgroundPattern.Height - y*1, ActualWidth, _backgroundPattern.Height));
+                context.DrawImage(_backgroundPattern.BitmapImage, new Rect(0, y * _backgroundPattern.Height - y, ActualWidth, _backgroundPattern.Height));
             }
 
             var bannerHeight = ActualHeight * .08;
 
             context.DrawImage(_banner.BitmapImage, new Rect(0,0, ActualWidth, bannerHeight));
             var tradeUiText = new FormattedText("TradeUI", CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
-                new Typeface("Arial"), 20, Brushes.White);
+                new Typeface("Arial"), 20, Brushes.Black);
             
             context.DrawText(tradeUiText, new Point(ActualWidth / 2 - tradeUiText.Width / 2, bannerHeight / 2));
+
+            var borderCount = ActualHeight / _borderLeft.Height;
+
+            for (var y = 0; y < borderCount; y++) {
+                context.DrawImage(_borderLeft.BitmapImage, new Rect(0, y * _borderLeft.Height, _borderLeft.Width, _borderLeft.Height));
+            }
         }
     }
 }
