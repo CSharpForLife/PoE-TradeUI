acc85ad2c35e025a5ec8d0550b39ee8e4dfbbd91
Jiiks <jiiks@windowslive.com>
2017-05-21 18:48:00 +0300

    Extensions

--- PoE-TradeUI/PoE-TradeUI.Utils/Extensions.cs
+++ PoE-TradeUI/PoE-TradeUI.Utils/Extensions.cs
@@ -4,8 +4,15 @@ using System.Windows.Media;
 namespace PoE_TradeUI.Utils {
     public static class Extensions {
 
-        public static void AddImage(this DrawingGroup drawingGroup, WpfImage image, Rect rect) {
-            drawingGroup.Children.Add(new ImageDrawing(image.BitmapImage, rect));
+        public static void AddImage(this DrawingGroup drawingGroup, WpfImage image, double x, double y, bool scale = true) {
+            AddImage(drawingGroup, image, x,y,scale ? image.ScaledWidth : image.Width, scale ? image.ScaledHeight : image.Height);
+        }
+        public static void AddImage(this DrawingGroup drawingGroup, WpfImage image, double x, double y, double width, double height) {
+            drawingGroup.Children.Add(new ImageDrawing(image.BitmapImage, new Rect(x, y, width, height)));
+        }
+
+        public static WpfImage ToWpfImage(this ImageDef def) {
+            return new WpfImage(def);
         }
 
     }
--- PoE-TradeUI/PoE-TradeUI.Wpf/ui/Background.xaml.cs
+++ PoE-TradeUI/PoE-TradeUI.Wpf/ui/Background.xaml.cs
@@ -7,13 +7,13 @@ namespace PoE_TradeUI.Wpf.ui {
     public partial class Background  {
 
         private readonly WpfImage
-        _backgroundPattern = new WpfImage(Defs.GetImageDefByName("Background Pattern")),
-        _banner = new WpfImage(Defs.GetImageDefByName("Banner")),
-        _borderLeft = new WpfImage(Defs.GetImageDefByName("Border Left")),
-        _borderRight = new WpfImage(Defs.GetImageDefByName("Border Right")),
-        _borderTop = new WpfImage(Defs.GetImageDefByName("Border Top")),
-        _cornerTl = new WpfImage(Defs.GetImageDefByName("Corner TL")),
-        _cornerTr = new WpfImage(Defs.GetImageDefByName("Corner TR"));
+        _backgroundPattern = Defs.GetImageDefByName("Background Pattern").ToWpfImage(),
+        _banner = Defs.GetImageDefByName("Banner").ToWpfImage(),
+        _borderLeft = Defs.GetImageDefByName("Border Left").ToWpfImage(),
+        _borderRight = Defs.GetImageDefByName("Border Right").ToWpfImage(),
+        _borderTop = Defs.GetImageDefByName("Border Top").ToWpfImage(),
+        _cornerTl = Defs.GetImageDefByName("Corner TL").ToWpfImage(),
+        _cornerTr = Defs.GetImageDefByName("Corner TR").ToWpfImage();
 
         public Background() {
             InitializeComponent();
@@ -22,32 +22,31 @@ namespace PoE_TradeUI.Wpf.ui {
         protected override void OnRender(DrawingContext context) {
 
             var drawingGroup = new DrawingGroup();
-
             /*Paint background*/
             var bgCount = ActualHeight / _backgroundPattern.ScaledHeight;
             for (var y = 0; y < bgCount; y++) {
-                drawingGroup.AddImage(_backgroundPattern, new Rect(0, y * _backgroundPattern.Height, ActualWidth * _backgroundPattern.ScaleX, _backgroundPattern.ScaledHeight));
+                drawingGroup.AddImage(_backgroundPattern, 0, y * _backgroundPattern.Height, ActualWidth * _backgroundPattern.ScaleX, _backgroundPattern.ScaledHeight);
             }
 
             /*Paint Banner*/
-            drawingGroup.AddImage(_banner, new Rect(0, _borderTop.Height, ActualWidth * _banner.ScaleX, ActualHeight * _banner.ScaleY));
+            drawingGroup.AddImage(_banner, 0, _borderTop.Height, ActualWidth * _banner.ScaleX, ActualHeight * _banner.ScaleY);
 
             /*Paint top border*/
             var topBorderCount = ActualWidth / _borderTop.ScaledWidth + 1;
             for (var x = 0; x < topBorderCount; x++) {
-                drawingGroup.AddImage(_borderTop, new Rect(x * _borderTop.Width - x, 0, _borderTop.ScaledWidth, _borderTop.ScaledHeight));
+                drawingGroup.AddImage(_borderTop, x * _borderTop.Width - x, 0);
             }
 
             /*Paint left and right border*/
             var leftBorderCount = ActualHeight / _borderLeft.ScaledHeight;
             for (var y = 0; y < leftBorderCount; y++) {
-                drawingGroup.AddImage(_borderLeft, new Rect(0, y * _borderLeft.Height, _borderLeft.ScaledWidth, _borderLeft.ScaledHeight));
-                drawingGroup.AddImage(_borderRight, new Rect(ActualWidth - _borderRight.Width, y * _borderRight.Height, _borderRight.ScaledWidth, _borderRight.ScaledHeight));
+                drawingGroup.AddImage(_borderLeft, 0, y * _borderLeft.Height);
+                drawingGroup.AddImage(_borderRight, ActualWidth - _borderRight.Width, y * _borderRight.Height);
             }
 
             /*Paint corners*/
-            drawingGroup.AddImage(_cornerTl, new Rect(0, 0, _cornerTl.ScaledWidth, _cornerTl.ScaledHeight));
-            drawingGroup.AddImage(_cornerTr, new Rect(ActualWidth - _cornerTr.Width, 0, _cornerTr.ScaledWidth, _cornerTr.ScaledHeight));
+            drawingGroup.AddImage(_cornerTl, 0, 0);
+            drawingGroup.AddImage(_cornerTr, ActualWidth - _cornerTr.Width, 0);
 
             drawingGroup.Freeze();
             context.DrawDrawing(drawingGroup);
