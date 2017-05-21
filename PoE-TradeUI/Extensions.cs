f2c16b856eda2f420128a2422c3bbfd84e602ee8
Jiiks <jiiks@windowslive.com>
2017-05-21 14:24:42 +0300

    Extension methods

---
+++ PoE-TradeUI/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using PoE_TradeUI.ui;
+
+namespace PoE_TradeUI {
+    public static class Extensions {
+
+        public static void AddImage(this DrawingGroup drawingGroup, Image image, Rect rect) {
+            drawingGroup.Children.Add(new ImageDrawing(image.BitmapImage, rect));
+        }
+
+    }
+}
