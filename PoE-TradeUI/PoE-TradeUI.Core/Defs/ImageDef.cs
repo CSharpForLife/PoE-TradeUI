c320ee8a56352f4d051aaa4f65c8ee6e2af21b10
Jiiks <jiiks@windowslive.com>
2017-05-23 09:34:11 +0300

    Extend def base

---
+++ PoE-TradeUI/PoE-TradeUI.Core/Defs/ImageDef.cs
@@ -0,0 +1,9 @@
+﻿namespace PoE_TradeUI.Core.Defs {
+    public class ImageDef : Def {
+        public string Format { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double ScaleY { get; set; }
+        public double ScaleX { get; set; }
+    }
+}
