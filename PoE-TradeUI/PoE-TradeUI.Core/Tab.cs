7c547a9556974628a0c5840c405e06cf8927b2d1
Jiiks <jiiks@windowslive.com>
2017-05-25 11:53:17 +0300

    Tab base

---
+++ PoE-TradeUI/PoE-TradeUI.Core/Tab.cs
@@ -0,0 +1,8 @@
+﻿namespace PoE_TradeUI.Core {
+    class Tab {
+        public string Title { get; set; }
+        public int Colour { get; set; }
+        public int Index { get; set; }
+        public string Url { get; set; }
+    }
+}
