dcb305f9fe7db6b9fcd7ea7cde0279bff7f08bd6
Jiiks <jiiks@windowslive.com>
2017-05-25 23:49:09 +0300

    Reusable poepanel, new resources and tests

---
+++ PoE-TradeUI/PoE-TradeUI.Wpf/ui/PoePanel.xaml.cs
@@ -0,0 +1,38 @@
+﻿using PoE_TradeUI.Core;
+using PoE_TradeUI.Core.Defs;
+using System.Windows.Media;
+
+namespace PoE_TradeUI.Wpf.ui {
+
+    public partial class PoePanel {
+
+        private readonly WpfImage _bordertl = Defs.GetImageDefByName("Border Top Left").ToWpfImage();
+        public ImageSource BorderTopLeft => _bordertl.BitmapImage;
+
+        private readonly WpfImage _borderTop = Defs.GetImageDefByName("Border Top").ToWpfImage();
+        public ImageSource BorderTop => _borderTop.BitmapImage;
+
+        private readonly WpfImage _bordertr = Defs.GetImageDefByName("Border Top Right").ToWpfImage();
+        public ImageSource BorderTopRight => _bordertr.BitmapImage;
+
+        private readonly WpfImage _borderRight = Defs.GetImageDefByName("Border Right").ToWpfImage();
+        public ImageSource BorderRight => _borderRight.BitmapImage;
+
+        private readonly WpfImage _borderbr = Defs.GetImageDefByName("Border Bottom Right").ToWpfImage();
+        public ImageSource BorderBottomRight => _borderbr.BitmapImage;
+
+        private readonly WpfImage _borderBottom = Defs.GetImageDefByName("Border Bottom").ToWpfImage();
+        public ImageSource BorderBottom => _borderBottom.BitmapImage;
+
+        private readonly WpfImage _borderbl = Defs.GetImageDefByName("Border Bottom Left").ToWpfImage();
+        public ImageSource BorderBottomLeft => _borderbl.BitmapImage;
+
+        private readonly WpfImage _borderLeft = Defs.GetImageDefByName("Border Left").ToWpfImage();
+        public ImageSource BorderLeft => _borderLeft.BitmapImage;
+
+        public PoePanel() {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
--- PoE-TradeUI/PoE-TradeUI.Wpf/ui/TabPopup.xaml.cs
+++ PoE-TradeUI/PoE-TradeUI.Wpf/ui/TabPopup.xaml.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Media;
+using PoE_TradeUI.Core;
+using PoE_TradeUI.Core.Defs;
 
 namespace PoE_TradeUI.Wpf.ui {
-    /// <summary>
-    /// Interaction logic for TabPopup.xaml
-    /// </summary>
-    public partial class TabPopup : UserControl {
+
+    public partial class TabPopup {
+
+        private readonly WpfImage _borderTop = Defs.GetImageDefByName("Border Top").ToWpfImage();
+        public ImageSource BorderTop => _borderTop.BitmapImage;
+        private readonly WpfImage _borderLeft = Defs.GetImageDefByName("Border Left").ToWpfImage();
+        public ImageSource BorderLeft => _borderLeft.BitmapImage;
+        private readonly WpfImage _borderRight = Defs.GetImageDefByName("Border Right").ToWpfImage();
+        public ImageSource BorderRight => _borderRight.BitmapImage;
+        private readonly WpfImage _cornerTl = Defs.GetImageDefByName("Corner TL").ToWpfImage();
+        public ImageSource CornerTopLeft => _cornerTl.BitmapImage;
+        private readonly WpfImage _cornerTr = Defs.GetImageDefByName("Corner TR").ToWpfImage();
+        public ImageSource CornerTopRight => _cornerTr.BitmapImage;
+
         public TabPopup() {
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
